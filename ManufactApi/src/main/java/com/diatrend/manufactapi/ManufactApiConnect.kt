abb730a43dfb6adec1677a4fad49cc44adc13966
hirano <thirano.hirano@gmail.com>
2020-12-15 18:02:51 +0900

    Second

--- ManufactApi/src/main/java/com/diatrend/manufactapi/ManufactApiConnect.kt
+++ ManufactApi/src/main/java/com/diatrend/manufactapi/ManufactApiConnect.kt
@@ -1,4 +1,42 @@
 package com.diatrend.manufactapi
 
-class ManufactApiConnect {
-}
+import com.diatrend.manufactapi.models.*
+import com.diatrend.manufactapi.service.ManufactApiRepository
+import retrofit2.Response
+import java.lang.Exception
+
+class ManufactApiConnect(server: String) {
+    private val manufactApiRepository = ManufactApiRepository(server)
+    var isConnect: Boolean = false
+
+    fun getLogClassList() : List<LogClass>? {
+        isConnect = false
+        return try {
+            val response = manufactApiRepository.getLogClassList()
+            if (response.isSuccessful) {
+                isConnect = true
+                response.body()
+            } else {
+                null
+            }
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    fun getBaseInfoLog(id: Int) : BaseInfoLogDetail? {
+        isConnect = false
+        return  try {
+            val response = manufactApiRepository.getBaseInfoLog(id)
+            if (response.isSuccessful) {
+                isConnect = true
+                response.body()
+            }
+            else {
+                null
+            }
+        } catch (e: Exception) {
+            null
+        }
+    }
+ }
